90d3bf6150c01c58c7ba875bbbb5daf3fc3fa48a
John Crisostomo <jdcrisostomo@gmail.com>
2011-07-25 18:31:26 +0800

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,11 @@
+#OwenTask
+
+Built in partial fulfillment of *Database Management Systems 1* (ITED123).
+
+*Professor* : Mr. Jeff Palma
+
+*Group Members* : Crisostomo, John Dominic
+                  Divinagracia, Joanel
+                  Garcia, Oeneri
+                  Landa, Arveen
+                  Yuson, Andrew
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OwenTasks::Application.config.secret_token = '3ac903003d666bfdb38ac8cfb9997fe418f5eed1c5e26cbedf5df73c739c81e470c75863d7eb94fe5a6e965c13944adccfb291e02007465d8de399deb5d39c17'
