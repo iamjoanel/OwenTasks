00cf372da86742399840513b89e0d3cbd827c562
John Crisostomo <jdcrisostomo@gmail.com>
2011-08-15 19:50:36 +0800

    finished with skeleton pages.

--- app/controllers/today_controller.rb
+++ app/controllers/today_controller.rb
@@ -1,5 +1,12 @@
 class TodayController < ApplicationController
+  before_filter :tangina_this
   def new
   end
 
+  private
+  def tangina_this
+    #render 'layouts/_header.html.erb'
+  end
+
 end
+
